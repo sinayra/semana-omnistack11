d2ee69474fd2ccf10adcba96aefe8d2b94e55dc1
sinayra <nayra@hotmail.co.jp>
2020-03-25 17:51:40 -0300

    Added nodemon

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+/**
+ * Métodos HTTP
+ * 
+ * GET: Buscar/listar informação no backend
+ * POST: Criar informação no backend
+ * PUT: Alterar informação no backend
+ * DELETE: Deletar informação no backend
+ */
+
+ /**
+  * Tipos de parâmetros
+  * 
+  * Query params: Parâmetros nomiados enviados na rota após "?" (filtros, paginação)
+  * Route params: Parâmetros utilizados para identificar recursos
+  * Request body: Corpo da requisição, utilizado para criar ou alterar recursos
+  */
+
 app.get('/', (request, response) => {
     return response.json({
         evento: 'Semana Omnistack'
