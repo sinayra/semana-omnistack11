a412a286be3a41d99d4b04921c3e078ffba723d3
sinayra <nayra@hotmail.co.jp>
2020-03-25 18:52:06 -0300

    List ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,12 @@ const crypto = require("crypto");
 const routes = express.Router();
 const conn = require("./database/connection");
 
+routes.get("/ongs", async (request, response) => {
+  const ongs = await conn("ongs").select("*");
+
+  return response.json({ ongs });
+});
+
 routes.post("/ongs", async (request, response) => {
   const { name, email, whatsapp, city, uf } = request.body;
   const id = crypto.randomBytes(4).toString("HEX");
