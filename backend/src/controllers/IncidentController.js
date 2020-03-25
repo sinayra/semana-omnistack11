33058e8b6c3d3fb38efe98c3118620b436af7264
sinayra <nayra@hotmail.co.jp>
2020-03-25 19:36:57 -0300

    Join with ONGs

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -22,12 +22,20 @@ module.exports = {
     const [count] = await conn("incidents").count();
 
     const ongs = await conn("incidents")
+      .join("ongs", "ongs.id", "=", "incidents.ong_id")
       .limit(5)
       .offset((page - 1) * 5)
-      .select("*");
+      .select([
+        "incidents.*",
+        "ongs.name",
+        "ongs.email",
+        "ongs.whatsapp",
+        "ongs.city",
+        "ongs.uf"
+      ]);
 
     response.header("X-Total-Count", count["count(*)"]);
-    
+
     return response.json({ ongs });
   },
 
