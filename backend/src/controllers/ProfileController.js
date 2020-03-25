16a8ebe7580ff2c855c34141ff150453ccd0adeb
sinayra <nayra@hotmail.co.jp>
2020-03-25 19:20:41 -0300

    List incidents from specific ong

---
+++ backend/src/controllers/ProfileController.js
@@ -0,0 +1,11 @@
+const conn = require("../database/connection");
+
+module.exports = {
+  
+  async index(request, response) {
+    const ong_id = request.headers.authorization;
+    const incidents = await conn("incidents").where("ong_id", ong_id).select("*");
+
+    return response.json({ incidents });
+  }
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,11 +2,15 @@ const express = require("express");
 const routes = express.Router();
 const OngController = require("./controllers/OngController");
 const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
 
 routes.post("/ongs", OngController.store);
 routes.get("/ongs", OngController.index);
+
 routes.post("/incidents", IncidentController.store);
 routes.get("/incidents", IncidentController.index);
 routes.delete("/incidents/:id", IncidentController.delete);
+
+routes.get("/profile", ProfileController.index);
 
 module.exports = routes;
