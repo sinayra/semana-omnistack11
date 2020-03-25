2f6adeb783215aa97346a9dfbda5b898cf75fb74
sinayra <nayra@hotmail.co.jp>
2020-03-25 18:48:21 -0300

    Insert into database

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,22 @@
 const express = require("express");
-
+const crypto = require("crypto");
 const routes = express.Router();
+const conn = require("./database/connection");
 
-routes.get('/', (request, response) => {
-    return response.json({
-        evento: 'Semana Omnistack'
-    });
+routes.post("/ongs", async (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body;
+  const id = crypto.randomBytes(4).toString("HEX");
+
+  await conn("ongs").insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  });
+
+  return response.json({ id });
 });
 
 module.exports = routes;
