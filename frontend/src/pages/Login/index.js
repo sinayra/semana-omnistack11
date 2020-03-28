df8d7b47b059521eaec77c8578c307820ea264a3
sinayra <nayra@hotmail.co.jp>
2020-03-28 17:27:17 -0300

    Base HTML for Login

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,6 +1,27 @@
-import React from "react"
+import React from "react";
+import { FiLogIn } from "react-icons/fi";
+
 import "./style.css"
+import heroesImg  from "../../assets/heroes.png";
+import logoImg  from "../../assets/logo.svg";
 
 export default function Login(){
-    return <h1>Hello</h1>
+    return (
+        <div className="login-container">
+            <section className="form">
+                <img src={logoImg} alt="Be the Hero"/>
+
+                <form>
+                    <h1>Faça o seu login</h1>
+
+                    <input placeholder="Sua ID" />
+                    <button type="submit">Entrar</button>
+
+                    <a href="#"><FiLogIn size="16" color="#E02041" /> Não tenho cadastro</a>
+                </form>
+            </section>
+
+            <img src={heroesImg} alt="Heroes"/>
+        </div>
+    );
 }
