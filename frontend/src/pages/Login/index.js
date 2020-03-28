4c1a30632ba1dc43744d777aa0ebc7946a422baa
sinayra <nayra@hotmail.co.jp>
2020-03-28 19:05:50 -0300

    Login ong

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiLogIn } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import api from "../../services/api";
 
 import "./style.css";
 import heroesImg from "../../assets/heroes.png";
 import logoImg from "../../assets/logo.svg";
 
 export default function Login() {
+  const [id, setID] = useState("");
+  const history = useHistory();
+
+  async function handleLogin(e){
+    e.preventDefault();
+
+    try {
+      const resp = await api.post("sessions", {id});
+
+      localStorage.setItem("ongID", id);
+      localStorage.setItem("ongName", resp.data.ong.name);
+
+      history.push("/profile");
+    } catch (err) {
+      alert(`Falha ao logar`);
+      console.log({ err });
+    }
+  }
+
   return (
     <div className="login-container">
       <section className="form">
         <img src={logoImg} alt="Be the Hero" />
 
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça o seu login</h1>
 
-          <input placeholder="Sua ID" />
+          <input
+            placeholder="Sua ID"
+            value={id}
+            onChange={e => setID(e.target.value)}
+          />
           <button className="button" type="submit">
             Entrar
           </button>
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -48,7 +48,7 @@ export default function Register() {
           </p>
 
           <Link className="back-link" to="/">
-            <FiArrowLeft size="16" color="#E02041" /> Não tenho cadastro
+            <FiArrowLeft size="16" color="#E02041" /> Voltar ao início
           </Link>
         </section>
 
