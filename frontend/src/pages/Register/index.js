b70c2cd66c03d19b3d9feeb074a88eed6bee3d4c
sinayra <nayra@hotmail.co.jp>
2020-03-28 18:03:08 -0300

    Fix link and space between

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -18,7 +18,7 @@ export default function Register() {
             encontrarem os casos da sua ONG.
           </p>
 
-          <Link className="back-link" to="/register">
+          <Link className="back-link" to="/">
             <FiArrowLeft size="16" color="#E02041" /> Não tenho cadastro
           </Link>
         </section>
