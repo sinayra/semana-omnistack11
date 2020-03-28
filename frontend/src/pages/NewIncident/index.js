0db69b61ea21c02b4213864462a214b23dbe7af2
sinayra <nayra@hotmail.co.jp>
2020-03-28 19:44:21 -0300

    Register new incident

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
+import api from "../../services/api";
 
-import './style.css';
+import "./style.css";
 import logoImg from "../../assets/logo.svg";
 
-export default function NewIncident () {
+export default function NewIncident() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState("");
+  const ongID = localStorage.getItem("ongID");
+  const history = useHistory();
+
+  async function handleNewInicident(e) {
+    e.preventDefault();
+
+    try {
+      await api.post(
+        "incidents",
+        {
+          title,
+          description,
+          value
+        },
+        {
+          headers: {
+            Authorization: ongID
+          }
+        }
+      );
+
+      history.push("/profile");
+    } catch (err) {
+      alert(`Erro ao realizar registro de caso, tente novamente mais tarde`);
+      console.log({ err });
+    }
+  }
+
   return (
     <div className="new-incident-container">
       <div className="content">
@@ -14,7 +46,8 @@ export default function NewIncident () {
 
           <h1>Cadastrar novo caso</h1>
           <p>
-            Descreva o caso detalhadamento para encontrar um heroi para resolver isso.
+            Descreva o caso detalhadamento para encontrar um heroi para resolver
+            isso.
           </p>
 
           <Link className="back-link" to="/profile">
@@ -22,10 +55,22 @@ export default function NewIncident () {
           </Link>
         </section>
 
-        <form>
-          <input placeholder="Título do caso" />
-          <textarea placeholder="Descrição"  />
-          <input placeholder="Valor em reais" />
+        <form onSubmit={handleNewInicident}>
+          <input
+            placeholder="Título do caso"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Descrição"
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+          />
+          <input
+            placeholder="Valor em reais"
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          />
 
           <button className="button" type="submit">
             Cadastrar
