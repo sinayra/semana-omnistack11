e9269fea5e8f43b4f56f1521048a9004743961c2
sinayra <nayra@hotmail.co.jp>
2020-03-28 18:01:35 -0300

    Register CSS

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -2,27 +2,31 @@ import React from "react";
 import { FiLogIn } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
-import "./style.css"
-import heroesImg  from "../../assets/heroes.png";
-import logoImg  from "../../assets/logo.svg";
+import "./style.css";
+import heroesImg from "../../assets/heroes.png";
+import logoImg from "../../assets/logo.svg";
 
-export default function Login(){
-    return (
-        <div className="login-container">
-            <section className="form">
-                <img src={logoImg} alt="Be the Hero"/>
+export default function Login() {
+  return (
+    <div className="login-container">
+      <section className="form">
+        <img src={logoImg} alt="Be the Hero" />
 
-                <form>
-                    <h1>Faça o seu login</h1>
+        <form>
+          <h1>Faça o seu login</h1>
 
-                    <input placeholder="Sua ID" />
-                    <button className="button" type="submit">Entrar</button>
+          <input placeholder="Sua ID" />
+          <button className="button" type="submit">
+            Entrar
+          </button>
 
-                    <Link to="/register"><FiLogIn size="16" color="#E02041" /> Não tenho cadastro</Link>
-                </form>
-            </section>
+          <Link className="back-link" to="/register">
+            <FiLogIn size="16" color="#E02041" /> Não tenho cadastro
+          </Link>
+        </form>
+      </section>
 
-            <img src={heroesImg} alt="Heroes"/>
-        </div>
-    );
+      <img src={heroesImg} alt="Heroes" />
+    </div>
+  );
 }
