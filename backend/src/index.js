1fcf6f0bf9bdf872c3937c3141f678f957c1cbf5
sinayra <nayra@hotmail.co.jp>
2020-03-29 20:40:33 -0300

    Send bad request if validations fails

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
+const { errors } = require("celebrate");
 
 const app = express();
 app.use(cors());
@@ -24,4 +25,5 @@ app.use(express.json());
   */
 
 app.use(routes);
+app.use(errors());
 app.listen(3333);
