71abb7807f1f9efe2f47ef2bb591227b732da411
sinayra <nayra@hotmail.co.jp>
2020-03-29 20:46:33 -0300

    Ignore unknows headers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -25,9 +25,9 @@ routes.get("/incidents", IncidentController.index);
 routes.delete("/incidents/:id", IncidentController.delete);
 
 routes.get("/profile", celebrate({
-    [Segments.HEADERS]: Joi.object().keys({
-        Authorization: Joi.string().required()
-    })
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
 }) ,ProfileController.index);
 
 module.exports = routes;
