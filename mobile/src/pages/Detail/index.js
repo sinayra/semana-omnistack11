54d01b00cdcb2202ca1e0640ed1d528fd793c04c
sinayra <nayra@hotmail.co.jp>
2020-03-29 16:47:46 -0300

    Navigation

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View } from "react-native";
 
-export default function Incidents() {
+export default function Details() {
     return (
         <View />
     );
