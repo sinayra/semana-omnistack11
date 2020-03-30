1593c5827853b387203270a5eed98ba0e5f087ad
sinayra <nayra@hotmail.co.jp>
2020-03-29 21:05:09 -0300

    Unit test

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,10 +1,10 @@
-const crypto = require("crypto");
 const conn = require("../database/connection");
+const generateUniqueID = require("../utils/generateUniqueID");
 
 module.exports = {
   async store(request, response) {
     const { name, email, whatsapp, city, uf } = request.body;
-    const id = crypto.randomBytes(4).toString("HEX");
+    const id = generateUniqueID();
 
     await conn("ongs").insert({
       id,
